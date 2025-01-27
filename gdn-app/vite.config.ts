d67acb1caa852e9040112cfd3bd9afef9358e146
Joscha <joscha@plugh.de>
2025-01-27 19:33:24 +0100

    Set up vue dev tools

--- gdn-app/vite.config.ts
+++ gdn-app/vite.config.ts
@@ -2,12 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
