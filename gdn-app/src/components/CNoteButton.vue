447bcf39a76b4ee3b9c20117ddbf128c043d0f4d
Joscha <joscha@plugh.de>
2025-02-10 02:10:20 +0100

    Fix pinned button border

--- gdn-app/src/components/CNoteButton.vue
+++ gdn-app/src/components/CNoteButton.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 <template>
   <div
     class="flex select-none items-center rounded-sm border p-0.5 transition hover:scale-110 active:scale-95"
-    :class="props.inverted ? 'bg-black text-white' : 'bg-white text-black'"
+    :class="props.inverted ? 'border-black bg-black text-white' : 'bg-white text-black'"
   >
     <slot></slot>
   </div>
