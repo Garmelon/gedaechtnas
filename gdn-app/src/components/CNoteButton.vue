7a58ac37f708f64c15ff3e0a3dc3efcb647e6c00
Joscha <joscha@plugh.de>
2025-02-11 19:10:25 +0100

    Fix rendering bug by not enlarging buttons
    Previously, the buttons left weird smudges when moving the mouse while a
    button was enlarged. It appears to be a rendering engine bug (webkit
    gtk), since the same does not happen in Firefox/Chromium. This fix
    avoids enlarging the buttons so no residue is left behind.

--- gdn-app/src/components/CNoteButton.vue
+++ gdn-app/src/components/CNoteButton.vue
@@ -6,8 +6,12 @@ const props = defineProps<{
 
 <template>
   <div
-    class="flex select-none items-center rounded-sm border p-0.5 transition hover:scale-110 active:scale-95"
-    :class="props.inverted ? 'border-black bg-black text-white' : 'bg-white text-black'"
+    class="flex select-none items-center rounded-sm border p-0.5 transition hover:bg-neutral-200 active:scale-95"
+    :class="
+      props.inverted
+        ? 'border-black bg-black text-white hover:bg-neutral-600'
+        : 'bg-white text-black'
+    "
   >
     <slot></slot>
   </div>
