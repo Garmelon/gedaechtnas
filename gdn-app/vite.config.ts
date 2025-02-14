ccfdd25ee906921b72092bae7a4f6cfc8083c760
Joscha <joscha@plugh.de>
2025-02-14 19:13:20 +0100

    Allow top-level await

--- gdn-app/vite.config.ts
+++ gdn-app/vite.config.ts
@@ -7,8 +7,16 @@ import vueDevTools from "vite-plugin-vue-devtools";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig(() => ({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+
+  esbuild: {
+    supported: {
+      // Should be fine, but doesn't appear to be the default yet:
+      // https://caniuse.com/mdn-javascript_operators_await_top_level
+      "top-level-await": true,
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -19,13 +27,7 @@ export default defineConfig(async () => ({
     port: 1420,
     strictPort: true,
     host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
+    hmr: host ? { protocol: "ws", host, port: 1421 } : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
