5e87d74ede087a1a8fe0b25af0342e69c74cb79f
Joscha <joscha@plugh.de>
2025-05-03 01:34:58 +0200

    Rename repo show command to repo info

--- gdn-cli/src/commands/repo.rs
+++ gdn-cli/src/commands/repo.rs
@@ -1,7 +1,7 @@
 mod add;
+mod info;
 mod list;
 mod remove;
-mod show;
 
 use clap::Parser;
 
@@ -13,8 +13,8 @@ pub enum Command {
     #[command(visible_alias = "l")]
     List(list::Command),
 
-    #[command(visible_alias = "s")]
-    Show(show::Command),
+    #[command(visible_alias = "i")]
+    Info(info::Command),
 
     #[command(visible_alias = "a")]
     Add(add::Command),
@@ -27,7 +27,7 @@ impl Command {
     pub fn run(self, env: &Environment) -> anyhow::Result<()> {
         match self {
             Self::List(command) => command.run(env),
-            Self::Show(command) => command.run(env),
+            Self::Info(command) => command.run(env),
             Self::Add(command) => command.run(env),
             Self::Remove(command) => command.run(env),
         }
